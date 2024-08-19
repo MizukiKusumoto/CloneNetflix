44438ac2637378eec754c9edf7b3218c3175522d
MizukiKusumoto <apple1167water@gmail.com>
2024-08-19 21:14:16 +0900

    20240819(2h)

--- clone-netflix/src/App.tsx
+++ clone-netflix/src/App.tsx
@@ -1,11 +1,25 @@
 import './App.css';
+import { Banner } from './components/Banner';
+import { Header } from './components/Header';
 import { Row } from './components/Row';
 import { requests } from './request';
 
 function App() {
   return (
     <div className="App">
-      <Row fetchUrl={requests.fetchNetflixOriginals} />
+      <Header />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGUINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} isLargeRow />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="News Movies" fetchUrl={requests.fetchNewsMovies} />
+      <Row title="Kids Movies" fetchUrl={requests.fetchKidsMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentMovies} />
     </div>
   );
 }
